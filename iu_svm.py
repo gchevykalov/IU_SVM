ddbe50ad197a62990aafeab0918062044dee2706
gchevykalov <71878379+gchevykalov@users.noreply.github.com>
2023-03-31 20:41:38 +0300

    Add files via upload

--- iu_svm.py
+++ iu_svm.py
@@ -6,7 +6,7 @@ INF = 1e12
 class IUSVMClassifier:
     def __init__(self, kernel, C):
         self.alphas = []
-        self.mu = 0
+        self.mu = np.array([0], np.float32)
         self.x = []
         self.y = []
         self.kernel = kernel
@@ -21,6 +21,10 @@ class IUSVMClassifier:
 
 
     def deleteFromSetS(self, index):
+        if len(self.S) == 1:
+            self.Q = np.array([[0]], np.float32)
+            self.S.remove(index)
+            return
         k = self.S.index(index) + 1
         q_12 = np.delete(self.Q[:,k].reshape(-1, 1), k, 0)
         q_21 = np.delete(self.Q[k,:].reshape(1, -1), k, 1)
@@ -31,16 +35,25 @@ class IUSVMClassifier:
 
 
     def addToSetS(self, index):
+        if len(self.S) == 0:
+            self.Q = np.array([[-self.y[index] * self.y[index] * self.kernel(self.x[index], self.x[index]), self.y[index]],[self.y[index], 0]], np.float32)
+            self.S.append(index)
+            #print(self.Q)
+            return
         nu = np.array([[self.y[index]] +
                       [self.y[s] * self.y[index] * self.kernel(self.x[index], self.x[s]) for s in self.S]],
                       np.float32).T
+        #print(nu)
         betas = np.append(-1 * self.Q @ nu, [[1]], 0)
+        #print(betas)
         k = self.y[index] * self.y[index] * self.kernel(self.x[index], self.x[index]) - nu.T @ self.Q @ nu
+        #print(k)
         self.Q = np.append(
             np.append(self.Q, np.array([[0] for i in range(len(self.S) + 1)]), 1),
             np.array([[0 for i in range(len(self.S) + 2)]]),
             0
         ) + 1 / k * betas @ betas.T
+        #print(self.Q)
         self.S.append(index)
 
 
@@ -74,6 +87,8 @@ class IUSVMClassifier:
                 deltaAmax.append((self.C - self.alphas[self.S[i - 1]]) / betas[i])
             elif betas[i] < -EPS:
                 deltaAmax.append(-self.alphas[self.S[i - 1]] / betas[i])
+            else:
+                deltaAmax.append(np.array([INF], np.float32))
 
         if len(deltaAmax) != 0:
             i = np.argmin(deltaAmax)
@@ -88,10 +103,14 @@ class IUSVMClassifier:
         for i in range(len(self.E)):
             if gammas[i + 1] > EPS:
                 deltas.append(-self.g[self.E[i]] / gammas[i + 1])
+            else:
+                deltas.append(np.array([INF], np.float32))
         shift = len(self.E)
         for i in range(len(self.O)):
             if gammas[i + 1 + shift] < -EPS:
                 deltas.append(-self.g[self.O[i]] / gammas[i + 1 + shift])
+            else:
+                deltas.append(np.array([INF], np.float32))
 
         if len(deltas) != 0:
             deltaAcR = min(deltas)
@@ -125,16 +144,16 @@ class IUSVMClassifier:
             if abs(self.g[e]) < EPS:
                 self.addToSetS(e)
                 self.E.remove(e)
-                return False
+                return
 
         for o in self.O:
             if abs(self.g[o]) < EPS:
                 self.addToSetS(o)
                 self.O.remove(o)
-                return False
+                return
 
         mus_r = []
-        for i in range(len(self.alphas)):
+        for i in range(len(self.alphas) - 1):
             if i in self.E and -(self.y[i] / self.y[-1]) > EPS:
                 mus_r.append(-self.g[i] / self.y[i])
             elif i in self.O and -(self.y[i] / self.y[-1]) < -EPS:
@@ -145,30 +164,29 @@ class IUSVMClassifier:
             mu_r = INF
         mu_c = -self.g[-1] / self.y[-1]
         mu = min(mu_r, mu_c)
-
+         
         for i in range(len(self.alphas)):
             self.g[i] += self.y[i] * mu
+        self.mu += mu
 
         for e in self.E:
             if abs(self.g[e]) < EPS:
                 self.addToSetS(e)
                 self.E.remove(e)
-                return False
+                return
 
         for o in self.O:
             if abs(self.g[o]) < EPS:
                 self.addToSetS(o)
                 self.O.remove(o)
-                return False
+                return
 
         self.addToSetS(len(self.alphas) - 1)
-        self.mu += mu
-        return True
 
 
     def incrementPair(self, x_c, y_c):
         alpha_c = 0
-        g_c = y_c * self(x_c) - 1
+        g_c = y_c * self(x_c) - (1 + EPS)
 
         self.x.append(x_c)
         self.y.append(y_c)
@@ -176,16 +194,16 @@ class IUSVMClassifier:
         self.g.append(g_c)
 
         while self.g[-1] < -EPS and self.alphas[-1] < self.C:
-            if len(self.S) == 0:
-                isNeedToLeave = self.processEmptySetS()
-                if isNeedToLeave:
-                    return
-
             betas = self.calculateBetas()
             gammas = self.calculateGammas(betas)
 
             delta_ac, k = self.calculateDelta(betas, gammas)
 
+            if delta_ac == 0:
+                if len(self.S) == 0:
+                    self.processEmptySetS()
+                    continue
+            
             self.mu += betas[0] * delta_ac
             for i in range(len(self.S)):
                 self.alphas[self.S[i]] += betas[i + 1] * delta_ac
@@ -215,14 +233,14 @@ class IUSVMClassifier:
                 else:
                     self.E.append(k)
                 break
-
+        
         new_index = len(self.x) - 1
         if new_index not in self.S and new_index not in self.E:
             self.O.append(new_index)
 
 
     def __call__(self, x):
-        res = self.mu
+        res = self.mu[0]
 
         for i in range(len(self.alphas)):
             res += self.alphas[i] * self.y[i] * self.kernel(self.x[i], x)
